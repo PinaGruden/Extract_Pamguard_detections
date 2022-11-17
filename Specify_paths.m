2c55e0d48e74a135a3259c3c025e3eaf0d98f826
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-11-16 16:08:35 -1000

    Create Specify_paths.m

---
+++ Specify_paths.m
@@ -0,0 +1,13 @@
+% Specify paths to your folders:
+
+%\\\\\\\\\\\\\\\\\ Specify database (.sqlite3) folder (with path) \\\\\\\\\\\\\\\\\\\\\\\\\
+sqldatabase='/Users/pinagruden/Dropbox/Pina/HAWAII/MATLAB/Ground_truth_fromJenn/Lasker_AC109/PAM20013b_HICEAS_Lasker_AC109_Trial-1b.sqlite3';
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+%\\\\ Specify Folder (with path) where you want extracted SQL tables (.csv) to be saved to \\\\
+sqltables = '/Users/pinagruden/Dropbox/Pina/HAWAII/MATLAB/Ground_truth_fromJenn/Lasker_AC109/Extracted_Tables/';
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+%\\\\ Specify Folder (with path) where you want Tables containing detection info to be saved to \\\\
+folder2save2_info = '/Users/pinagruden/Dropbox/Pina/HAWAII/MATLAB/Ground_truth_fromJenn/Lasker_AC109/';
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
